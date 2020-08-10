720f91c36d1f55a41aa7be42b422e303a8dacadf
Fewing <yangzheyu98@gmail.com>
2020-08-10 17:05:14 +0800

    创建页面路由

--- src/src/router/index.js
+++ src/src/router/index.js
@@ -10,6 +10,30 @@ Vue.use(VueRouter)
     name: 'Home',
     component: Home
   },
+  {
+    path: '/welcome',
+    name: 'Welcome',
+    meta: {tittle:'欢迎页',},
+    component: () => import('../views/Welcome.vue')
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    meta: {title: '注册',},
+    component: () => import('../views/Register.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    meta: {title: '登录',},
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: 'doc/:id',
+    name:'Document',
+    meta: {tittle:'文档',},
+    component: () => import('../views/Doc.vue')
+  }
 ]
 
 const router = new VueRouter({
