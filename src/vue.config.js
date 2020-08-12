f0b70974e5e4f3f1683be180d9f382ca256c748a
Li-Zhijian2000 <lzj01071508@buaa.edu.cn>
2020-08-12 16:31:25 +0800

    alias

--- src/vue.config.js
+++ src/vue.config.js
@@ -1,19 +1,29 @@
 // vue.config.js for less-loader@6.0.0
+
+const path = require('path');
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+
 module.exports = {
-    css: {
-      loaderOptions: {
-        less: {
-          lessOptions: {
-            modifyVars: {
-              'primary-color': '#E85A4F',
-              'link-color': '#ec583a',
-              'btn-border-radius-sm': '0px',
-              'btn-border-radius-base': '0px',
-              'card-radius': '10px',
-            },
-            javascriptEnabled: true,
+  css: {
+    loaderOptions: {
+      less: {
+        lessOptions: {
+          modifyVars: {
+            'primary-color': '#E85A4F',
+            'link-color': '#ec583a',
+            'btn-border-radius-sm': '0px',
+            'btn-border-radius-base': '0px',
+            'card-radius': '10px',
           },
+          javascriptEnabled: true,
         },
       },
     },
-  };
+  },
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@', resolve('src'))
+  },
+};
