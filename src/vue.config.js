55fa40c7cdcbd1a9b99fed9610d9a0fd472e4da3
Colanns <mycolands@gmail.com>
2020-08-12 16:46:26 +0800

    修复了冲突

--- src/vue.config.js
+++ src/vue.config.js
@@ -6,7 +6,6 @@ function resolve(dir) {
 }
 
 module.exports = {
-<<<<<<< HEAD
   css: {
     loaderOptions: {
       less: {
@@ -15,22 +14,8 @@ module.exports = {
             'primary-color': '#E85A4F',
             'link-color': '#ec583a',
             'btn-border-radius-sm': '0px',
-            'btn-border-radius-base': '0px',
+            //'btn-border-radius-base': '0px',还是有个圆角好看一点
             'card-radius': '10px',
-=======
-    css: {
-      loaderOptions: {
-        less: {
-          lessOptions: {
-            modifyVars: {
-              'primary-color': '#E85A4F',
-              'link-color': '#ec583a',
-              'btn-border-radius-sm': '0px',
-              //'btn-border-radius-base': '0px',还是有个圆角好看一点
-              'card-radius': '10px',
-            },
-            javascriptEnabled: true,
->>>>>>> 733ee62b7ed8d57a8df68f0fbe8094d2b61a183c
           },
           javascriptEnabled: true,
         },
