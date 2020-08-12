512bff277e1fa5526c2817cc859cd7872200996e
Colanns <mycolands@gmail.com>
2020-08-12 21:39:02 +0800

    修改了权限按钮相关的样式

--- src/src/views/Doc.vue
+++ src/src/views/Doc.vue
@@ -3,29 +3,43 @@
         <div style="margin:24px 48px 0 0;text-align:right">
             <a-popover placement="bottomLeft" trigger="click">
                 <template slot="content" style="text-align:right">
-                    <div style="margin-top:10px">
-                        <a-input placeholder="https://asasaasd"/>
-                        
-                        <a-checkbox>团队仅查看</a-checkbox>
-                        <a-checkbox>团队可编辑</a-checkbox>
-                        <a-divider style="margin-top:10px;margin-bottom:10px"/>
-                        <a-checkbox style="margin-bottom:-10px">允许团队评论</a-checkbox>
+                    <div style="margin-top:12px">
+                        <a-radio-group v-model="groupAuthValue" @change="onChange">
+                            <a-radio :value="1">
+                                允许团队查看
+                            </a-radio>
+                            <a-radio :value="2">
+                                允许团队编辑
+                            </a-radio>
+                        </a-radio-group>
+                        <a-divider style="margin-top:24px;margin-bottom:10px"/>
+                        <a-radio style="margin-bottom:0px">允许团队评论</a-radio>
                     </div>
-                
-                    <a-button style="margin:24px 10px 10px 0px">确认</a-button>
+                    <div style="text-align:right">
+                        <a-button style="margin:24px 10px 10px 0px">取消</a-button>
+                        <a-button type="primary" style="margin:24px 10px 10px 0px">确认</a-button>
+                    </div>    
                 </template>
                 <a-button style="margin-right:24px"><a-icon type="safety"/>团队权限</a-button>
             </a-popover>
             <a-popover placement="bottomLeft" trigger="click">
-                <template slot="content" style="text-align:right">
-                    <div>
-                        <a-checkbox>仅查看</a-checkbox>
-                        <a-checkbox>可编辑</a-checkbox>
-                        <a-divider style="margin-top:10px;margin-bottom:10px"/>
-                        <a-checkbox style="margin-bottom:-10px">确认</a-checkbox>
+                <template slot="content">
+                    <div style="margin-top:12px" >
+                        <a-radio-group v-model="personAuthValue" @change="onChange">
+                            <a-radio :value="1">
+                                允许查看
+                            </a-radio>
+                            <a-radio :value="2">
+                                允许编辑
+                            </a-radio>
+                        </a-radio-group>
+                        <a-divider style="margin-top:24px;margin-bottom:10px"/>
+                        <a-radio style="margin-bottom:0px">允许评论</a-radio>
                     </div>
-                
-                    <a-button style="margin:24px 10px 10px 0px">分享链接</a-button>
+                    <div style="text-align:right">
+                        <a-button type="primary" style="margin:24px 10px 10px 0px">分享链接</a-button>
+                    </div>
+                    
                 </template>
                 <a-button @click="openShare"><a-icon type="share-alt"/>分享</a-button>
             </a-popover>
@@ -47,8 +61,8 @@
     },
     data() {
       return {
-        s1: '1234',
-        s2: '5678'
+        groupAuthValue: 0,
+        personAuthValue: 0,
       }
     }
   }
