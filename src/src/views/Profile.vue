80aad63cf167b38eb0969e31864ba3f088cf905c
Fewing <yangzheyu98@gmail.com>
2020-08-16 12:34:22 +0800

    上传头像基本完成（但很丑）

--- src/src/views/Profile.vue
+++ src/src/views/Profile.vue
@@ -2,12 +2,20 @@
   <div class="profile">
     <a-breadcrumb style="margin:100px 0 -100px -600px;">
       <a-breadcrumb-item>右墨文档</a-breadcrumb-item>
-      <a-breadcrumb-item><a href="/">工作台</a></a-breadcrumb-item>
+      <a-breadcrumb-item>
+        <a href="/">工作台</a>
+      </a-breadcrumb-item>
       <a-breadcrumb-item>账户</a-breadcrumb-item>
     </a-breadcrumb>
     <a-layout class="profileBox">
       <a-layout-sider style="text-align:center;background:#F3F3F3">
-        <a-avatar :size="64" style="margin-top:120px" :src="data.avatar"></a-avatar>
+        <a-avatar :size="64" v-if="data.avatar!=null" style="margin-top:120px" :src="data.avatar"></a-avatar>
+        <avatar
+          :size="64"
+          v-if="data.avatar==null"
+          :username="`${data.username}`"
+          style="margin-top:120px;margin-left:auto;margin-right:auto"
+        ></avatar>
         <a-menu
           style="background:#F3F3F3;margin-top:50px"
           :default-selected-keys="['1']"
@@ -15,76 +23,96 @@
           mode="inline"
           @click="handleClick"
         >
-          <a-menu-item key="1">
-            账号信息
-          </a-menu-item>
-          <a-menu-item key="2">
-            设置
-          </a-menu-item>
-      </a-menu>
+          <a-menu-item key="1">账号信息</a-menu-item>
+          <a-menu-item key="2">头像</a-menu-item>
+        </a-menu>
       </a-layout-sider>
-      <a-layout-content style="background:#fff;text-align:left;">
-        <h1 style="margin:48px 0 0 64px"><b>账户信息</b></h1>
+      <a-layout-content style="background:#fff;text-align:left;" v-if="sider_status==1">
+        <h1 style="margin:48px 0 0 64px">
+          <b>账户信息</b>
+        </h1>
         <div style="margin:12px 0 0 64px">
           <a-row style="margin-top:24px">
             <a-col :span="10">
-              <b><a-icon type="user"/> 账户</b>
+              <b>
+                <a-icon type="user" />账户
+              </b>
             </a-col>
-            <a-col :span="6">
-              {{data.username}}
-            </a-col>
+            <a-col :span="6">{{data.username}}</a-col>
             <!-- <a-col :span="6">
               <a-button type="link" style="margin-top:-5px" @click="handleUpdateInfo">修改</a-button>
-            </a-col> -->
+            </a-col>-->
           </a-row>
           <a-row style="margin-top:24px">
             <a-col :span="10">
-              <b><a-icon type="info-circle"/> 简介</b>
+              <b>
+                <a-icon type="info-circle" />简介
+              </b>
             </a-col>
-            <a-col :span="6">
-              {{data.description}}
-            </a-col>
+            <a-col :span="6">{{data.description}}</a-col>
             <!-- <a-col :span="6">
               <a-button type="link" style="margin-top:-5px">修改</a-button>
-            </a-col> -->
+            </a-col>-->
           </a-row>
           <a-row style="margin-top:24px">
             <a-col :span="10">
-              <b><a-icon type="phone"/> 手机</b>
+              <b>
+                <a-icon type="phone" />手机
+              </b>
             </a-col>
-            <a-col :span="6">
-              {{data.phone}}
-            </a-col>
+            <a-col :span="6">{{data.phone}}</a-col>
             <!-- <a-col :span="6">
               <a-button type="link" style="margin-top:-5px">修改</a-button>
-            </a-col> -->
+            </a-col>-->
           </a-row>
           <a-row style="margin-top:24px">
             <a-col :span="10">
-              <b><a-icon type="mail"/> 邮箱</b>
+              <b>
+                <a-icon type="mail" />邮箱
+              </b>
             </a-col>
-            <a-col :span="6">
-              {{data.email}}
-            </a-col>
+            <a-col :span="6">{{data.email}}</a-col>
             <!-- <a-col :span="6">
               <a-button type="link" style="margin-top:-5px">修改</a-button>
-            </a-col> -->
+            </a-col>-->
           </a-row>
           <a-row style="margin-top:24px">
             <a-col :span="10">
-              <b><a-icon type="calendar"/> 生日</b>
+              <b>
+                <a-icon type="calendar" />生日
+              </b>
             </a-col>
-            <a-col :span="6">
-              {{ birthday }}
-            </a-col>
+            <a-col :span="6">{{ birthday }}</a-col>
             <!-- <a-col :span="6">
               <a-button type="link" style="margin-top:-5px">修改</a-button>
-            </a-col> -->
+            </a-col>-->
           </a-row>
           <a-row style="margin-top:30px;">
             <a-button type="primary" @click="handleUpdateInfo">修改个人信息</a-button>
             <a-button type="primary" @click="handleUpdatePassword" style="margin-left: 20px;">修改密码</a-button>
           </a-row>
+        </div>
+      </a-layout-content>
+      <a-layout-content
+        v-if="sider_status==2"
+        style="background:#fff;text-align:center;"
+      >
+        <div>
+          <a-upload
+            name="image"
+            list-type="picture-card"
+            class="avatar-uploader"
+            :show-upload-list="false"
+            action="http://182.92.57.178:5000/pictures/add"
+            :before-upload="beforeUpload"
+            @change="handleChange"
+          >
+            <img v-if="data.avatar!=null" :src="data.avatar" alt="avatar" />
+            <div v-else>
+              <a-icon :type="loading ? 'loading' : 'plus'" />
+              <div class="ant-upload-text">Upload</div>
+            </div>
+          </a-upload>点击即可上传头像
         </div>
       </a-layout-content>
     </a-layout>
@@ -93,14 +121,11 @@
         <a-form-item label="手机号">
           <a-input v-model="temp.phone" />
         </a-form-item>
-        <a-form-item label="生日" >
+        <a-form-item label="生日">
           <a-date-picker v-model="temp.birthday" format="YYYY/MM/DD" style="width: 100%;" />
         </a-form-item>
         <a-form-item label="邮箱">
           <a-input v-model="temp.email" />
-        </a-form-item>
-        <a-form-item label="头像">
-          <a-input v-model="temp.avatar" />
         </a-form-item>
         <a-form-item label="个人描述">
           <a-input v-model="temp.description" />
@@ -110,27 +135,27 @@
     <a-modal v-model="updatePasswordModalVisible" title="修改密码" center @ok="updatePassword">
       <a-form :label-col="labelCol" :wrapper-col="wrapperCol" :form="temp">
         <a-form-item label="旧密码">
-          <a-input v-model="temp.old_password"/>
+          <a-input v-model="temp.old_password" />
         </a-form-item>
         <a-form-item label="新密码">
-          <a-input v-model="temp.new_password"/>
+          <a-input v-model="temp.new_password" />
         </a-form-item>
       </a-form>
     </a-modal>
   </div>
 </template>
 <style>
-.profile{
-  background:#FAFAFA;
-  text-align:center;
-  position:flex;
+.profile {
+  background: #fafafa;
+  text-align: center;
+  position: flex;
 }
 
-.profileBox{
-  height:400px;
-  width:800px;
-  margin:120px auto;
-  box-shadow: 0 0 5px hsla( 0,0%,0%,.1 );
+.profileBox {
+  height: 400px;
+  width: 800px;
+  margin: 120px auto;
+  box-shadow: 0 0 5px hsla(0, 0%, 0%, 0.1);
 }
 
 .card-container {
@@ -143,17 +168,24 @@
 }
 </style>
 <script>
-import Vue from 'vue'
+import Vue from "vue";
+import Avatar from "vue-avatar";
+
 export default {
   name: "Home",
-  components: {},
+  components: {
+    Avatar,
+  },
   data() {
     return {
       openKeys: [],
       data: {},
       temp: {},
+      sider_status: 1,
       updateInfoModalVisible: false,
       updatePasswordModalVisible: false,
+      loading: false,
+      imageUrl: "",
       labelCol: {
         xs: { span: 24 },
         sm: { span: 5 },
@@ -162,13 +194,13 @@ export default {
         xs: { span: 24 },
         sm: { span: 12 },
       },
-    }
+    };
   },
   computed: {
-    birthday: function() {
-      const date = this.data.birthday
-      return date ? date.split('T')[0] : ''
-    }
+    birthday: function () {
+      const date = this.data.birthday;
+      return date ? date.split("T")[0] : "";
+    },
   },
   watch: {
     openKeys(val) {
@@ -176,88 +208,110 @@ export default {
     },
   },
   mounted() {
-    this.getInfo()
+    this.getInfo();
   },
   methods: {
+    handleChange(info) {
+      if (info.file.status === "uploading") {
+        this.loading = true;
+        return;
+      }
+      if (info.file.status === "done") {
+        // Get this url from response in real world.
+        this.imageUrl = info.file.response.url;
+        Vue.axios({
+          method: "put",
+          url: "http://39.106.230.20:8090/user/mod_info",
+          headers: {
+            token: this.$store.state.token,
+          },
+          data: {
+            avatar: this.imageUrl,
+          },
+        }).then((response) => {
+          console.log(response.data);
+          this.getInfo();
+          this.$message.success("头像上传成功", 1.5);
+          this.loading = false;
+        });
+      }
+    },
+    beforeUpload(file) {
+      const isJpgOrPng =
+        file.type === "image/jpeg" || file.type === "image/png";
+      if (!isJpgOrPng) {
+        this.$message.error("You can only upload JPG file!");
+      }
+      const isLt2M = file.size / 1024 / 1024 < 2;
+      if (!isLt2M) {
+        this.$message.error("Image must smaller than 2MB!");
+      }
+      return isJpgOrPng && isLt2M;
+    },
     getInfo() {
       Vue.axios({
-        method: 'get',
-        url: 'http://39.106.230.20:8090/user/info',
+        method: "get",
+        url: "http://39.106.230.20:8090/user/info",
         headers: {
           token: this.$store.state.token,
         },
-      }).then(response => {
-        console.log(response.data)
-        this.data = response.data
-      })
+      }).then((response) => {
+        this.data = response.data;
+        console.log(response.data.avatar);
+        this.imageUrl == response.data.avatar;
+        console.log(this.imageUrl);
+      });
     },
     handleUpdateInfo() {
-      const { phone, birthday, email, avatar, description } = this.data
-      this.temp = {
+      const { phone, birthday, email, description } = this.data;
+      (this.temp = {
         phone,
         birthday,
         email,
-        avatar,
-        description
-      },
-      this.updateInfoModalVisible = true
+        description,
+      }),
+        (this.updateInfoModalVisible = true);
     },
     updateInfo() {
       Vue.axios({
-        method: 'put',
-        url: 'http://39.106.230.20:8090/user/mod_info',
+        method: "put",
+        url: "http://39.106.230.20:8090/user/mod_info",
         headers: {
           token: this.$store.state.token,
         },
-        data: this.temp
-      }).then(response => {
-        console.log(response.data)
-        this.getInfo()
-        this.$message.success("修改成功", 1.5)
-      })
-      this.updateInfoModalVisible = false
+        data: this.temp,
+      }).then((response) => {
+        console.log(response.data);
+        this.getInfo();
+        this.$message.success("修改成功", 1.5);
+      });
+      this.updateInfoModalVisible = false;
     },
     handleUpdatePassword() {
       this.temp = {
-        old_password: '',
-        new_password: ''
-      }
-      this.updatePasswordModalVisible = true
+        old_password: "",
+        new_password: "",
+      };
+      this.updatePasswordModalVisible = true;
     },
     updatePassword() {
       Vue.axios({
-        method: 'put',
-        url: 'http://39.106.230.20:8090/user/mod_password',
+        method: "put",
+        url: "http://39.106.230.20:8090/user/mod_password",
         headers: {
           token: this.$store.state.token,
         },
-        data: this.temp
-      }).then(response => {
-        console.log(response.data)
-        this.getInfo()
-        this.$message.success("修改成功", 1.5)
-      })
-      this.updatePasswordModalVisible = false
+        data: this.temp,
+      }).then((response) => {
+        console.log(response.data);
+        this.getInfo();
+        this.$message.success("修改成功", 1.5);
+      });
+      this.updatePasswordModalVisible = false;
     },
     handleClick(e) {
       console.log("click", e);
-      this.stopmanage();
-      if (e.key == "doc") {
-        this.sider_status = 1;
-      }
-      if (e.key[0] == "t") {
-        this.sider_status = 2;
-        var current_teamid=e.key.substring(1);
-        for (let index = 0; index < this.teams.length; index++) {
-          const element = this.teams[index];
-          if (element.teamid==current_teamid) {
-            this.current_team=this.teams[index];
-          }
-        }
-      }
-      if (e.key == "trash") {
-        this.sider_status = 3;
-      }
+      this.sider_status = e.key;
     },
     startmanage() {
       this.ismanage = true;
@@ -299,5 +353,19 @@ export default {
 /* .slide-fade-leave-active for below version 2.1.8 */ {
   transform: translateX(10px);
   opacity: 0;
+}
+.avatar-uploader > .ant-upload {
+  margin-left: 128px;
+  width: 128px;
+  height: 128px;
+}
+.ant-upload-select-picture-card i {
+  font-size: 32px;
+  color: #999;
+}
+
+.ant-upload-select-picture-card .ant-upload-text {
+  margin-top: 8px;
+  color: #666;
 }
 </style>
